c74536801ebf1ff790022f7a6ce108aa530add13
Ryan Hood <ryanhood@Ryans-MacBook-Pro.local>
2023-10-29 04:21:06 -0500

    added footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,20 +15,40 @@ function Footer() {
      
       <div className='lg:col-span-3 flex justify-between space-x-10'>
             <div>
-            <h1 className='w-full text-3xl font-bold text-[#00df9a]'> Contact Information</h1>
-            <h2 className='text-2xl'>Steve Kroeger</h2>
-            <p className='text-lg py-4'>615.377.9779</p>
-            <p className='text-lg'>SteveKroeger@email.com</p>
+            <h1 className='w-full text-3xl font-bold text-center text-[#00df9a]'> Find Us </h1>
+            <nav>
+                        <ul className="flex justify-between md:w-[75%] my-6">
+                            <li>
+                                <Link to="https://www.linkedin.com/company/akira-web-solutions/">
+                                    <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="https://www.facebook.com/profile.php?id=61550844571239">
+                                    <FontAwesomeIcon icon={faFacebook} size="2x" />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/Contact">
+                                    <FontAwesomeIcon icon={faEnvelope} size="2x" />
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
 
          
       </div>
 
-        <div>
-        <h2 className='text-2xl'>Steve Kroeger</h2>
+      <div>
+      <h2 className='text-2xl'>Gavin O'neil</h2>
             <p className='text-lg py-4'>615.377.9779</p>
-            <p className='text-lg'>SteveKroeger@email.com</p>
-        </div>
+            <p className='text-lg'>GavinOniel@email.com</p>
 
+      </div>
+
+      <div></div>
+
+       
       </div>
     </footer>
   );
