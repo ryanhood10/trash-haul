8936a08e489cb0c1843a3519fe5a6a8a7cbcfae5
Ryan Hood <ryanhood@ryans-mbp.lan>
2023-10-30 15:59:09 -0500

    fixed header and hero mobile styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,15 +15,22 @@ const Header = () => {
 
   return (
     <header>
-      <nav className='flex justify-between  items-center h-24 max-w-[1240px] mx-auto'>
+      <nav className='flex justify-between  md:text-sm items-center h-24 max-w-[1240px] mx-auto'>
         <Link to="/" className={location.pathname === '/' ? 'active' : ''}>
-        <h1 className='text-3xl font-bold text-green-700 m-4'> 
-        <img className="h-40 inline-block" src={Logo} alt="Our Logo" ></img>Nashville Trash Haul Co.</h1>
+        <h1 className='pt-4 text-md font-bold text-green-700 m-4 flex md:hidden'>
+          <img className="h-24 md:h-40 inline-block" src={Logo} alt="Our Logo" ></img>
+          Nashville Trash Haul Co.
+        </h1>
+        <h1 className='pt-4 text-3xl lg:text-4xl font-bold  text-green-700 m-4 hidden md:block fixed-header'>
+          <img className="h-24 md:h-40 inline-block" src={Logo} alt="Our Logo" ></img>
+          Nashville Trash Haul Co.
+        </h1>
+
             </Link>
 
           <div>
-            <p className='text-lg pr-8 pt-4'>615-476-9950</p>
-            <p className='text-lg pr-8'>GavinOneil@email.com</p>
+            <p className='text-sm pr-8 pt-4'>615-476-9950</p>
+            <p className='text-sm pr-8'>GavinOneil@email.com</p>
 
             </div>    
       </nav>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,8 +9,8 @@ const Hero = () => {
             <div className='max-h-[500px] relative'>
                 {/* overlay */}
                 <div className='absolute w-full h-full rounded-3xl text-gray-200 max-h-[500px] bg-black/20'>
-                    <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:7xl font-bold' >The Best <span className='text-yellow-400'> Trash Removal</span></h1>
-                    <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:7xl font-bold' >in <span className='text-yellow-400'> Nashville TN</span></h1>
+                    <h1 className='px-4 text-2xl sm:text-5xl md:text-6xl lg:7xl font-bold' >The Best <span className='text-yellow-400'> Trash Removal</span></h1>
+                    <h1 className='px-4 text-2xl sm:text-5xl md:text-6xl lg:7xl font-bold' >in <span className='text-yellow-400'> Nashville TN</span></h1>
                 </div>
                     <img className='w-full rounded-3xl max-h-[500px] object-cover' src={robot1}  alt="robot1" />
             </div>
